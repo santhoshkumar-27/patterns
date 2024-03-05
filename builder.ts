2c29b05b99a3c3b886895530a419cf1c542b9405
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-05 17:50:04 +0530

    feat: builder pattern

---
+++ builder.ts
@@ -0,0 +1,52 @@
+// builder pattern
+
+class Burger {
+    constructor(
+        public bun: string,
+        public ketchup: boolean,
+        public mustrad: boolean,
+        public kraut: boolean,
+    ) {
+    }
+}
+
+
+
+const semBurger = new Burger('wheat', true, true, true);
+
+
+
+class BurgerBuilder {
+
+    private ketchUp: boolean;
+    private mustrad: boolean;
+    private kraut: boolean;
+
+    constructor (private bun: string) {
+    }
+
+    addKetchUp() {
+        this.ketchUp = true;
+        return this;
+    }
+
+    addMustrad() {
+        this.mustrad = true;
+        return this;
+    }
+
+    addKraut() {
+        this.kraut = true;
+        return this;
+    }
+
+    bake() {
+        console.log('im will be ready in 10 mins')
+    }
+}
+
+
+const burger2 = new BurgerBuilder('wheat')
+
+
+burger2.addKetchUp().addKraut().addMustrad().bake()
