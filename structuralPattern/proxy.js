1e2a106313dcfd78a1b095c7beab23975076a3ad
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-09 12:26:09 +0530

    fix: proxy pattern

---
+++ structuralPattern/proxy.js
@@ -0,0 +1,24 @@
+// proxy is like a substitue for a anything
+/*
+    but in the programming the there is proxy for the real object
+    you replace target object with the proxy object
+*/
+
+
+class proxy {
+
+    constructor(data, callbackFn) {
+
+    }
+}
+
+
+const data = {
+    key: 'string',
+    value: 'number'
+}
+
+
+// its in the vue js
+
+const proxer = new proxy(data)
