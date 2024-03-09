d085c3f5f12548a736c7d6da4650463a46f738a4
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-09 20:15:26 +0530

    feat: added iterator pattern

--- behavioralpattern/iterator.js
+++ behavioralpattern/iterator.js
@@ -7,3 +7,34 @@ const cart = ['apple', 'banana', 'orange'];
 for (const item of cart) {
     console.log('item', item);
 }
+
+
+// define custom iterator pattern
+// of range functions
+function range(start, end, step = 1) {
+    return {
+        [Symbol.iterator]() {
+            return this
+        },
+        next() {
+
+            if (start < end) {
+                start += step;
+
+                return {
+                    done: false,
+                    value: start
+                }
+            }
+
+            return {
+                done: true,
+                value: end
+            }
+        }
+    }
+}
+
+for (const item of range(1, 5, 1)) {
+    console.log('item', item);
+}
