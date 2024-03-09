25dca0f5b147513f7e74f6389cb2650a15bfaafa
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-09 12:42:40 +0530

    fix: creating proxy obect

--- structuralPattern/proxy.js
+++ structuralPattern/proxy.js
@@ -5,14 +5,6 @@
 */
 
 
-class proxy {
-
-    constructor(data, callbackFn) {
-
-    }
-}
-
-
 const data = {
     key: 'string',
     value: 'number'
@@ -21,4 +13,14 @@ const data = {
 
 // its in the vue js
 // reactive system in the vue.js intercept data and updated the UI changes, using the proxy of data
-const proxer = new proxy(data)
+// when ever the data changes, with substitue of data
+const proxer = new Proxy(data, {
+    get(target, key) {
+        // get the value from the key
+        return target[key];
+    },
+    set(target, key, value) { // set to use to re-render the ui
+        console.log('updated the ui')
+        return Reflect.set(target, key, value) // and update to original data
+    }
+})
