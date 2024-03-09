c77ba2dd57bfe4a2c6dd11f259d1c6dae373083f
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-09 20:07:15 +0530

    fix: iterator pattern

---
+++ behavioralpattern/iterator.js
@@ -0,0 +1,9 @@
+// iterator pattern means
+
+// traverse through the collection of objects or items
+
+const cart = ['apple', 'banana', 'orange'];
+
+for (const item of cart) {
+    console.log('item', item);
+}
