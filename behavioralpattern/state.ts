3466642da499eb3f81647930501a401b8620902f
santhoshkumar <its.santhoshkumar@outlook.com>
2024-03-10 20:24:44 +0530

    fix: state pattern

--- behavioralpattern/state.ts
+++ behavioralpattern/state.ts
@@ -14,3 +14,45 @@ class Human {
         }
     }
 }
+
+
+interface State {
+    think(): string;
+}
+
+class HappyState implements State {
+    think(): string {
+        return 'I am happy';
+    }
+}
+
+class SadState implements State {
+    think(): string {
+        return 'I am Sad';
+    }
+}
+
+
+class NormalState implements State {
+    think(): string {
+        return 'I am normal';
+    }
+}
+
+
+class HumanBeing {
+    state: State;
+
+    constructor() {
+        this.state = new HappyState();
+    }
+
+    think() {
+        console.log(this.state.think())
+    }
+
+
+    changeState(state: State) {
+        this.state = state;
+    }
+}
